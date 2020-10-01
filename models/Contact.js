96bc1ab34e92c321ba0fe6c3b27f270f7dac307e
elliottparedes <elliottparedes@yahoo.com>
2020-10-01 02:05:12 -0500

    final update for contact page

--- models/Contact.js
+++ models/Contact.js
@@ -39,7 +39,7 @@ Contact.prototype.send = function()
         from: "contactus@moneymatters.com",
         to: process.env.EMAIL,
         subject: this.subject,
-        text: this.email +" "+ "said " + this.message
+        text: this.email +" "+ "said: " + this.message
     
     }
 
